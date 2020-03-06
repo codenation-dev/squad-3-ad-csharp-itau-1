69bb4b7db96dc404a3cfcdb089ea586fbd7e919c
Rafael CP <rafael_it@hotmail.com>
2020-03-06 18:37:41 -0300

    feat: inclusão do esqueleto do projeto MessageBroker

---
+++ src/MessageBroker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryLog.MessageBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

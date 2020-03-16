51d05d6e8a5ef915c27938463f42476880386f4a
Rafael CP <rafael_it@hotmail.com>
2020-03-16 20:37:12 -0300

    feat: add IUsuario interface to user entity

--- src/Core/Entities/Usuario.cs
+++ src/Core/Entities/Usuario.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TryLog.Core.Entities
 {
     public class Usuario
     {
-        public long Id { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string Token { get; set; }
-        public bool IsAtivo { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public bool IsRemoved { get; set; }
+        public long Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public string Token { get; private set; }
+        public bool IsAtivo { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public bool IsRemoved { get; private set; }
+    }
+    public interface IUsuario
+    {
+        Usuario Create(string email, string nome, string senha);
+        IEnumerable<Usuario> Get(Func<Usuario, bool> predicate);
+        long Remove(Func<Usuario, bool> predicate);
+
     }
 }

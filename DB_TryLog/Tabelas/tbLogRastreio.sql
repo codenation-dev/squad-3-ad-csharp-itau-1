01c446579cef40fa941839347c9cf0525eb0e069
Moyses Souza <moyses.souza@outlook.com.br>
2020-03-26 22:57:36 -0300

    Delete diretório DB_TryLog
    A documentação complementar do projeto será colocada no Confluence, para detahes sobre o TryLog consultar o README.md.

--- DB_TryLog/Tabelas/tbLogRastreio.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[tbLogRastreio]
-(
-	[Id] INT NOT NULL  IDENTITY, 
-    [Id_tbLog] INT NOT NULL, 
-    [Id_tbSituacao] INT NOT NULL, 
-    [DataHora] DATETIME NOT NULL DEFAULT GetDate(), 
-    CONSTRAINT [PK_tbLogRastreio] PRIMARY KEY ([Id])
-)

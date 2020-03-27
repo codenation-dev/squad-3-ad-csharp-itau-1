495c1e6a08c62efd48b6076b2f2c8fe91601161b
Moyses Souza <moyses.souza@outlook.com.br>
2020-03-27 17:36:14 -0300

    feat ModelandoEventos

---
+++ src/Core/Model/EventDetailHardware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryLog.Infraestructure.Model
+{
+    public class EventDetailHardware
+    {
+        public long Id { get; set; }
+        public long IdEvent { get; set; }
+        public string RamMemory { get; set; }
+        public string Processor { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Disk { get; set; }
+        public string Resolution { get; set; }
+        public string AdressMac { get; set; }
+        public string DeviceName { get; set; }
+    }
+}
---
+++ src/Core/Model/Layer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryLog.Infraestructure.Model
+{
+    public class Layer
+    {
+        public short Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Active { get; set; }
+        public DateTime DateRegister { get; set; }
+        public string Deleted { get; set; }
+    }
+}

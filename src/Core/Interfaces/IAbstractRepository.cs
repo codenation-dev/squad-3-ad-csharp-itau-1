268e266e2717cc8705bf500c14b96eed77e11614
Rafael CP <rafael_it@hotmail.com>
2020-03-27 17:27:57 -0300

    feat: adiciona interfaces básicas para inicio das atividades

---
+++ src/Core/Interfaces/IAbstractRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TryLog.Core.Interfaces
+{
+    public interface IAbstractRepository<T>
+    {
+        T Get(T entity);
+        List<T> Find(Expression<Func<T, bool>> predicate);
+        void SaveOrUpdate(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ src/Core/Interfaces/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TryLog.Core.Entities;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TryLog.Core.Interfaces
+{
+    public interface IEventRepository : IAbstractRepository<Event>
+    {
+        
+    }
+}

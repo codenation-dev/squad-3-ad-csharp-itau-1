9a079087823c50cf8de7a4b070677f86b889e521
Maira <mairamourabarbosa@gmail.com>
2020-03-29 13:36:04 -0300

    EventRepository added, Entity folder deleted, User.cs moved to Model

---
+++ src/Infraestructure/Repository/EventRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using TryLog.Core.Entities;
+using TryLog.Core.Interfaces;
+using TryLog.Infraestructure.Model;
+
+namespace TryLog.Infraestructure.Repository
+{
+    class EventRepository : IEventRepository
+    {
+        public void Delete(Event entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> Find(Expression<Func<Event, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event Get(Event entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOrUpdate(Event entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

157926e06715eee6bd6e7dd3a7d4f6d0162d1431
Rafael CP <rafael_it@hotmail.com>
2020-03-29 21:05:32 -0300

    feat: adiciona contexto EF e EventoRepository ao container de injeção de dependência

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -6,18 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TryLog.Core.Interfaces;
+using TryLog.Infraestructure.EF;
+using TryLog.Infraestructure.Repository;
 
 namespace TryLog.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +32,15 @@ namespace TryLog.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development"){
+                services.AddDbContext<TryLogContext>(op => op.UseInMemoryDatabase("TryLogDb.db"));
+            }
+            else
+            {
+                services.AddDbContext<TryLogContext>(op => op.UseSqlServer(Configuration.GetConnectionString("TryLogDb")));
+            }
+            services.AddScoped<IEventRepository,EventRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

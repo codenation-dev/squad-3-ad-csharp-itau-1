caa1c756118ae6d87419ca653ee6b16f00588da1
Rafael CP <rafael_it@hotmail.com>
2020-03-29 21:07:41 -0300

    feat: add contexto EF

---
+++ src/Infraestructure/EF/TryLogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TryLog.Core.Interfaces;
+using TryLog.Core.Model;
+
+namespace TryLog.Infraestructure.EF
+{
+    public class TryLogContext : DbContext
+    {
+        
+        public TryLogContext(DbContextOptions<TryLogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}

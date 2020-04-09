40920afa2a30f490a4f2ef5ca6121b45cdab88f6
Maira <mairamourabarbosa@gmail.com>
2020-04-09 08:18:30 -0300

    SaveOrUpdate event method added to EventManagerUC class.

---
+++ src/UseCase/EventManagerUC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using TryLog.Core.Interfaces;
+using TryLog.Core.Model;
+
+namespace TryLog.UseCase
+{
+    public class EventManagerUC
+    {
+        private readonly IEventRepository _repoEvent;
+        public EventManagerUC(IEventRepository eventRepository)
+        {
+            _repoEvent = eventRepository;
+        }
+        public void UpdateStatus(string IdEvent, string updateStatus)
+        {
+            long.TryParse(IdEvent, out var IdEventLong);
+            //var evento = _repoEvent.Find(x => x.Id == IdEventLong).FirstOrDefault();
+
+            /*evento.Status = new Core.Model.Status()
+            {
+                Description = updateStatus,
+            };
+            
+            _repoEvent.SaveOrUpdate(evento);*/
+        }
+
+        public Event SaveOrUpdate(long idEvent)
+        {
+            var evento = _repoEvent.Find(x => x.Id == idEvent).FirstOrDefault();
+
+            if (evento.Id == idEvent)
+            {
+                throw new ArgumentException($"Event ID '{idEvent}' already exists.");
+            }
+
+            _repoEvent.SaveOrUpdate(evento);
+
+            return evento;
+        }
+    }
+}

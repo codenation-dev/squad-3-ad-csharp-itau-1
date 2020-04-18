bd7043288c5e16121246b54635c2e1b55b9ac214
Maira <mairamourabarbosa@gmail.com>
2020-04-18 17:34:45 -0300

    Added 'AddIdentity options' for Lockout, Password, User and SignIn.

--- src/WebApi/Controllers/V1/VersionController.cs
+++ src/WebApi/Controllers/V1/VersionController.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
-using System.Web;
-using TryLog.Core.Interfaces;
 using TryLog.Core.Model;
-using TryLog.Infraestructure.EF;
-using TryLog.Infraestructure.Repository;
 using TryLog.UseCase;
 
 namespace TryLog.WebApi.Controllers.V1
@@ -27,16 +22,13 @@ namespace TryLog.WebApi.Controllers.V1
         [AllowAnonymous]
         public async Task<ActionResult<object>> Get()
         {
-            User newUser= new User("Pedro", "pe", "pedro@gmail.com", "Pedro12!", DateTime.Now, DateTime.Now);
+            User newUser = new User("Pedro", "pe", "pedro@gmail.com", "Pedro12!", DateTime.Now, DateTime.Now);
             if (TryValidateModel(newUser))
             {
-               return await _userUC.Create(newUser);
+                return await _userUC.Create(newUser);
 
             }
-
-
             return "Tudo Certo!";
-
         }
     }
 }

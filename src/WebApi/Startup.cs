76522db7b03cf7d8cab55e41511b7d23db37bd83
Rafael CP <rafael_it@hotmail.com>
2020-05-10 17:24:03 -0300

    fix: coloca appsettings correto e garante existencia do banco

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -118,6 +118,11 @@ namespace TryLog.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<TryLogContext>();
+                context.Database.EnsureCreated();
+            }
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

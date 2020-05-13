98586d46c9c29c980f0d08c3ef84ee220532e0fe
Maira <mairamourabarbosa@gmail.com>
2020-05-13 16:38:50 -0300

    UnitTests

---
+++ tests/Sentinela/Comparers/EnvironmentIDComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryLog.Sentinela.Comparers
+{
+    public class EnvironmentIDComparer : IEqualityComparer<Core.Model.Environment>
+    {
+        public bool Equals(Core.Model.Environment x, Core.Model.Environment y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Core.Model.Environment obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}

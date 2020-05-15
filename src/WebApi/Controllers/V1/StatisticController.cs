c627f376e2b56f41e19aed607311ce0a35010d29
Rafael CP <rafael_it@hotmail.com>
2020-05-14 23:59:42 -0300

    feat: adiciona controller de estatistica

---
+++ src/WebApi/Controllers/V1/StatisticController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TryLog.Services.Interfaces;
+using TryLog.Services.ViewModel;
+
+namespace TryLog.WebApi.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+        private readonly IStatisticsService _service;
+        public StatisticController(IStatisticsService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Statistics
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.Statistics());
+        }        
+    }
+}
